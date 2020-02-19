24254d57babaf00cbe0d56189c27c56f31dd6e03
Enrique David Jose Incio Chapilliquen <enrique.incio@gmail.com>
2020-02-19 16:58:12 -0500

    Testing AWS

---
+++ SolutionAWSLambda/AWS.EVSoft.Dominio/Models/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWS.EVSoft.Dominio.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+    }
+
+    public class PersonaData {
+
+        public static IList<Persona> Personas { get; set; }
+
+        static PersonaData() {
+            Personas = new List<Persona>();
+            Personas.Add(new Persona { Id = 1, Apellido = "Incio Chapilliquen", Nombre = "Enrique" });
+            Personas.Add(new Persona { Id = 2, Apellido = "Alfaro Moron", Nombre = "David" });
+            Personas.Add(new Persona { Id = 3, Apellido = "Lopez Quiroz", Nombre = "Jose" });
+        }
+        
+    }
+}
---
+++ SolutionAWSLambda/AWSServerless.EVSoft.WebApi/Controllers/PersonaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AWS.EVSoft.Dominio.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSServerless.EVSoft.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        public IList<Persona> Personas { get { return PersonaData.Personas; } }
+        public IActionResult GetPersona() {
+
+            return BadRequest(Personas);
+
+        }
+    }
+}
